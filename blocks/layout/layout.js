201ef543d20286d534067877faa6dce9f7108d3f
yura <yura@sv.ru>
2017-02-14 13:48:45 +0300

    change jade

--- blocks/layout/layout.js
+++ blocks/layout/layout.js
@@ -1,18 +1,9 @@
-function MobileDetect() {
-	var UA = navigator.userAgent.toLowerCase();
-	return (/android|webos|iris|bolt|mobile|iphone|ipad|ipod|iemobile|blackberry|windows phone|opera mobi|opera mini/i.test(UA)) ? true : false;
-}
-jQuery(document).ready(function ($) {
-	if (!MobileDetect()) {
-		$nav = $(".layout-fixed");
-		$window = $(window);
-		$h = $nav.offset().top;
-		$window.scroll(function () {
-			if ($window.scrollTop() > $h) {
-				$nav.addClass("layout-fixed-active");
-			} else {
-				$nav.removeClass("layout-fixed-active");
-			}
-		});
-	}
-});
+    jQuery(function ($) {
+    	$(window).scroll(function () {
+    		if ($(this).scrollTop() > 500) {
+    			$('.layout-fixed').addClass('layout-fixed-active');
+    		} else if ($(this).scrollTop() < 200) {
+    			$('.layout-fixed').removeClass('layout-fixed-active');
+    		}
+    	});
+    });
--- blocks/nav/nav.js
+++ blocks/nav/nav.js
@@ -1,3 +1,6 @@
-$('.js-nav--menuCallSubmenu').click(function () {
+if (document.body.clientWidth < 992) {
+	$('.js-nav--menuLink').attr('href', '#');
+	$('.js-nav--menuCallSubmenu').click(function () {
 	$('.nav--submenu').toggleClass('nav--submenu-active');
-});
+	});
+}
